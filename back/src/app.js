016407e017a271d661f500028f2be30279617e90
Loris <loris.ec@outlook.com>
2022-06-06 10:53:01 +0200

    add cors support

--- back/src/app.js
+++ back/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 
 require("dotenv").config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const router = express.Router()
 
